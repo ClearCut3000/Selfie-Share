5e6b2fd74fd1861fc008cb08e9dffdffd9292107
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-03 22:17:59 +0300

    Importing photos

--- Selfie Share/ViewController.swift
+++ Selfie Share/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+  //MARK: - Properties
+  var images = [UIImage]()
+
+  //MARK: - UIView lifacycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    title = "Selfie Share"
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
   }
 
+  //MARK: - Methods
+  @objc func importPicture() {
+    let picker = UIImagePickerController()
+    picker.allowsEditing = true
+    picker.delegate = self
+    present(picker, animated: true)
+  }
 
+  //MARK: - UIImagePickerControllerDelegate Methods
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let image = info[.editedImage] as? UIImage else { return }
+    dismiss(animated: true)
+    images.insert(image, at: 0)
+    collectionView.reloadData()
+  }
+
+  //MARK: - UICollectionViewController Methods
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return images.count
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageView", for: indexPath)
+    if let imageView = cell.viewWithTag(1000) as? UIImageView {
+      imageView.image = images[indexPath.item]
+    }
+    return cell
+  }
 }
 
