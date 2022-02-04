5fc3de4807eebc7c7c2bb5375408a28efa4d0883
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-04 22:43:39 +0300

    MCSession, MCBrowserViewController

--- Selfie Share/ViewController.swift
+++ Selfie Share/ViewController.swift
@@ -5,18 +5,25 @@
 //  Created by Николай Никитин on 03.02.2022.
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
 
   //MARK: - Properties
   var images = [UIImage]()
+  var peerID = MCPeerID(displayName: UIDevice.current.name)
+  var mcSession: MCSession?
+  var mcAdvertiserAssistant: MCAdvertiserAssistant?
 
   //MARK: - UIView lifacycle
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Selfie Share"
+    navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPromt))
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+    mcSession?.delegate = self
   }
 
   //MARK: - Methods
@@ -25,6 +32,26 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     picker.allowsEditing = true
     picker.delegate = self
     present(picker, animated: true)
+  }
+
+  @objc func showConnectionPromt() {
+    let alert = UIAlertController(title: "Connect to others.", message: nil, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+    alert.addAction(UIAlertAction(title: "Join session", style: .default, handler: joinSession))
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    present(alert, animated: true)
+  }
+
+  func startHosting(ction: UIAlertAction) {
+    guard let mcSession = mcSession else { return }
+    mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-selfieshare", discoveryInfo: nil, session: mcSession)
+  }
+
+  func joinSession(action: UIAlertAction) {
+    guard let mcSession = mcSession else { return }
+    let mcBrowser = MCBrowserViewController(serviceType: "hws-selfieshare", session: mcSession)
+    mcBrowser.delegate = self
+    present(mcBrowser, animated: true)
   }
 
   //MARK: - UIImagePickerControllerDelegate Methods
